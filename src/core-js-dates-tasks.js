8fd2fbf77d6f6add5990af58a175984213caff78
Michael5366 <qwentin536@gmail.com>
2025-03-24 01:43:10 +0300

    feat: add formatDate solution (Mon, Mar 24, 2025 01:43 AM)

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -144,8 +144,29 @@ function isDateInPeriod(date, period) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const currentDate = new Date(date);
+
+  const month = currentDate.getUTCMonth() + 1;
+  const day = currentDate.getUTCDate();
+  const year = currentDate.getUTCFullYear();
+
+  const hours24 = currentDate.getUTCHours();
+  const min = currentDate.getUTCMinutes();
+  const sec = currentDate.getUTCSeconds();
+
+  let hour = hours24 % 12;
+
+  if (hour === 0) {
+    hour = 12;
+  }
+
+  const amOrPm = hours24 >= 12 ? 'PM' : 'AM';
+
+  const minSet = min.toString().padStart(2, 0);
+  const secSet = sec.toString().padStart(2, 0);
+
+  return `${month}/${day}/${year}, ${hour}:${minSet}:${secSet} ${amOrPm}`;
 }
 
 /**
